ed608c412f6be8b595e5a37226ef6694ed4c7c11
MutazTB <91382419+MutazTB@users.noreply.github.com>
2022-03-13 00:07:24 +0300

    IsPerfect

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		static void Main(string[] args)
+		{
+			//int[] perfect = { 1, 2, 3 };
+			//Console.WriteLine(IsPerfect(perfect));
+		}
+
+        static string IsPerfect(int[] sequence)
+        {
+
+            int product = 1;
+            int sum = 0;
+            foreach (int number in sequence)
+            {
+                if (number < 0)
+                {
+                    return "No";
+                }
+                product *= number;
+                sum += number;
+            }
+            if (product == sum)
+            {
+                return "Yes";
+            }
+            return "No";
+        }
+    }
+}
