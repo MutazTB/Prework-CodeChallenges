96daa6e9ac405da35033ba49922eed4d32e71c92
MutazTB <91382419+MutazTB@users.noreply.github.com>
2022-03-12 23:55:56 +0300

    IsLeapYear

--- Program.cs
+++ Program.cs
@@ -6,8 +6,22 @@ public class Class1
 	{
 		static void Main(string[] args)
 		{
-			Console.ReadLine();
-			Console.WriteLine("Hello World!");
+			// Console.WriteLine(IsLeapYear());// call the IsLeapYear function and get the output
 		}
-	}
+
+        static string IsLeapYear()
+        {
+            Console.WriteLine("Enter Year : ");
+            int Year = int.Parse(Console.ReadLine());
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+            {
+                return "Yes it is a Leap Year.";
+            }
+            else
+            {
+                return "No it is not a Leap Year.";
+            }
+        }
+
+    }
 }
